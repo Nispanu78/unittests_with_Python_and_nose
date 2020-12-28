63241c4f3594e29564e122c72ee4598304e33d36
Nicola <nicspanu@gmail.com>
2020-12-28 16:30:01 +0100

    5

--- test_regular_expressions_with_nose.py
+++ test_regular_expressions_with_nose.py
@@ -23,3 +23,22 @@ class Item(object):
     def __init__(self, item, price):
         self.item = item
         self.price = price
+
+
+import unittest
+class ShoppingCartTest(unittest.TestCase):
+    def setUp(self):
+        self.cart = ShoppingCart().add("tuna sandwich", 15.00)
+
+    def length(self):
+        self.assertEquals(1, len(self.cart))
+
+    def test_item(self):
+        self.assertEquals("tuna sandwich", self.cart.item(1))
+
+    def test_price(self):
+        self.assertEquals(15.00, self.cart.price(1))
+
+    def test_total_with_sales_tax(self):
+        self.assertAlmostEquals(16.39,
+        self.cart.total(9.25), 2)
